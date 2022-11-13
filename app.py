84c1faa251c13e93efc9c0ed2280c6704896e7ee
Osinkolu <femiosinkolu@gmail.com>
2022-11-13 01:16:04 +0100

    minor

--- app.py
+++ app.py
@@ -171,11 +171,11 @@ def main():
         sns.heatmap(corr, cmap="Greens")
         plt.title('Heatmap of similarities between all the job descriptions')
         st.pyplot(fig)
-        st.write("\n The Similarity score between Job descriptions 1 and 2 is {}%".format(corr["job description 1".format(i)][1]))
-        st.write("The Similarity score between Job descriptions 1 and 3 is {}%".format(corr["job description 1".format(i)][2]))
-        st.write("The Similarity score between Job descriptions 1 and 4 is {}%".format(corr["job description 1".format(i)][3]))
-        st.write("The Similarity score between Job descriptions 1 and 5 is {}%".format(corr["job description 1".format(i)][4]))
-        st.write("The Similarity score between Job descriptions 1 and 6 is {}%\n".format(corr["job description 1".format(i)][5]))
+        st.write("\n The Similarity score between Job descriptions 1 and 2 is {}%".format(corr["job description 1"][1]))
+        st.write("The Similarity score between Job descriptions 1 and 3 is {}%".format(corr["job description 1"][2]))
+        st.write("The Similarity score between Job descriptions 1 and 4 is {}%".format(corr["job description 1"][3]))
+        st.write("The Similarity score between Job descriptions 1 and 5 is {}%".format(corr["job description 1"][4]))
+        st.write("The Similarity score between Job descriptions 1 and 6 is {}%\n".format(corr["job description 1"][5]))
 
 
         # plot the WordCloud image                      
