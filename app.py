f5d73fa634e760d63c620a4c3c3ea02c3fb2a459
Victor Morris <92633344+Victorrmorris@users.noreply.github.com>
2023-01-18 18:35:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def similarity_score(job_description_a, job_description_b, model):
 def plot_word_cloud(text_list):
   comment_words = ''
   stopwords = set(STOPWORDS)
-if st.button("Submit")
+    if st.button("Submit")
 
 
   for val in text_list:
